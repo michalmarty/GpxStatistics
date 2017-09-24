91d47dab1de8a6254eb18a942bdefc9d9ae59aba
michalmarty <michal.martyniak@gmail.com>
2017-09-24 13:06:34 +0200

    adding XmlDataReader.cs

---
+++ GPXFileStatistics/XmlDataReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace GPXFileStatistics
+{
+    public class XmlDataReader : IBikeDataReader
+
+
+    {
+        public List<Coordinate> ReadData()
+        {
+            var maps = from c in XElement.Load("activity.xml").Elements("trkpt lat")
+                       select c;
+            List<Coordinate> xml = new List<Coordinate>();
+
+            foreach (var item in maps)
+            {
+                xml.Add(new Coordinate()
+                {
+                    Point = item.Element("trkpt").Value,
+                    Height = item.Element("ele").Value,
+                    Time = item.Element("time").Value,
+                });
+            }
+            return xml;
+        }
+
+
+    }
+}
